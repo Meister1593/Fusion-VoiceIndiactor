5155ef5963dad4ad319dd07c3a83cfe3b549259c
Meister1593 <leruop@gmail.com>
2023-06-17 04:50:32 +0500

    Experimental cube in front of head

--- Fusion-VoiceIndicator/VoiceIndicator.cs
+++ Fusion-VoiceIndicator/VoiceIndicator.cs
@@ -13,24 +13,17 @@ namespace FusionVoiceIndicator
         public static void Initialize()
         {
             Head = GameObject.Find("TrackingSpace/Head");
-            var distanceFromHead = 10;
-            VoiceIndicatorObject = new GameObject()
-            {
-                name = VoiceIndicatorName
-            };
+            VoiceIndicatorObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            VoiceIndicatorObject.name = VoiceIndicatorName;
+            VoiceIndicatorObject.transform.localPosition = Vector3.forward * 1.5f;
             VoiceIndicatorObject.transform.SetParent(Head.transform);
-            VoiceIndicatorObject.transform.localPosition = Vector3.forward * distanceFromHead;
 
-            var scaleRoot = new GameObject()
-            {
-                name = "ScaleRoot"
-            };
-            scaleRoot.transform.SetParent(VoiceIndicatorObject.transform);
-            scaleRoot.transform.localPosition = Vector3.zero;   // change scale pos here (locally)
-
-            var voiceVolumeMeshGo = GameObject.CreatePrimitive(PrimitiveType.Quad);
-            voiceVolumeMeshGo.transform.SetParent(scaleRoot.transform);
-            voiceVolumeMeshGo.transform.position = Vector3.up * 1.5f;
+            var mat = new Material(Shader.Find("Universal Render Pipeline/Lit (PBR Workflow)"));
+            mat.color = Color.red;
+            VoiceIndicatorObject.GetComponent<MeshRenderer>().material = mat;
+            VoiceIndicatorObject.GetComponent<MeshRenderer>().materials = new Material[] { mat };
+            VoiceIndicatorObject.GetComponent<MeshRenderer>().sharedMaterial = mat;
+            VoiceIndicatorObject.GetComponent<MeshRenderer>().sharedMaterials = new Material[] { mat };
             MelonLogger.Msg("Initialized VoiceIndicator");
         }
         public static void Uninitialize()
